31aa6ec7e2825320e961f700b1952742da7ccef9
nunomiguel22 <37627936+nunomiguel22@users.noreply.github.com>
2020-11-19 00:55:15 +0000

    a

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -17,39 +17,8 @@ $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "git@github.com:username/reponame.git";
 $BRANCH             = "master";
 
-$allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
-	'195.37.139.','193.174.' // VZG
-);
-$allowed = false;
 
-$headers = apache_request_headers();
-
-if (@$headers["X-Forwarded-For"]) {
-    $ips = explode(",",$headers["X-Forwarded-For"]);
-    $ip  = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-foreach ($allowed_ips as $allow) {
-    if (stripos($ip, $allow) !== false) {
-        $allowed = true;
-        break;
-    }
-}
-
-if (!$allowed) {
-	header('HTTP/1.1 403 Forbidden');
- 	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-    echo "</pre>\n</body>\n</html>";
-    exit;
-}
-
-flush();
-
-
-//if ( $_POST['payload'] ) {
+if ( $_POST['payload'] ) {
   // Only respond to POST requests from Github
 
 
@@ -60,5 +29,5 @@ flush();
     exec("cd {$LOCAL_REPO} && sudo -u reav git pull");
     die("done " . mktime());
   }
-//}
+}
 ?>
